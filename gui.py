ee71b8827b4cf498c1ec7ac15f10cd2858633965
Marc Rey <roxxistic@gmail.com>
2019-04-25 18:10:48 +0200

    adds ability to insert various components in canvas

--- gui.py
+++ gui.py
@@ -25,22 +25,20 @@ def assembleComponents(positions):
         ""
     ]
     screen = []
-    padding = 100
+    padding = 1
     initialpadding = 0
     while initialpadding < padding:
         screen.append("")
         initialpadding = initialpadding + 1
-    title = prepareTitle()
-    for i in title:
-        screen.append(i)
-    board = prepareBoard(positions)
-    for i in board:
-        screen.append(i)
-    renderInto(screen, 150, padding + 30, [], 0, 0, 0, 0)
+    renderInto(screen, 150, padding + 20, [], 0, 0, 0, 0)
+    # insert from top to bottom and from right to left, because of escaped chars counting problems!
+    renderInto(screen, 150, padding + 20, prepareTitle(), padding + 0, 0, 40, 5)
+    renderInto(screen, 150, padding + 20, prepareHistory(), padding + 6, 40, 40, 15)
+    renderInto(screen, 150, padding + 20, prepareBoard(positions), padding + 6, 0, 40, 15)
     return screen
 
 def prepareTitle():
-    """"""
+    """Shows the game title. Returns a list of lines to be rendered."""
     title = [
         " _____ _                             ",
         "|  _  | |___ _ _ ___ ___ ___ _ _ ___ ",
@@ -51,14 +49,9 @@ def prepareTitle():
     return title
 
 def prepareBoard(positions):
-    """"""
+    """Shows the Alquerque/Checkers board. Returns a list of lines to be rendered."""
     colLabels    = "     0   1   2   3   4"
     rowSeparator = "   +---+---+---+---+---+"
-    rowConnector = "   + | ╳ | ╳ | ╳ | ╳ | +"
-    conn = [
-        "╲\|/",
-        ""
-    ]
     board = []
     board.append(colLabels)
     board.append(rowSeparator)
@@ -85,15 +78,17 @@ def prepareCell(value):
 
 
 def prepareHistory():
-    """"""
+    """Returns some lines containing the game history."""
     return [
+        "HISTORY:",
+        "----------------",
         "E0 to B4",
         "something",
         "K5 to W22             "
     ]
 
 def renderInto(whole, wwidth, wheight, part, ptop, pleft, pwidth, pheight):
-    """"""
+    """Define the size of a canvas to be rendered and place some lines of content at a specific position."""
     # if whole does not comply to the defined width and height, then extend or shorten
     if (len(whole) > wheight):
         whole = whole[:wheight]
@@ -101,7 +96,7 @@ def renderInto(whole, wwidth, wheight, part, ptop, pleft, pwidth, pheight):
         delta = wheight - len(whole)
         i = 0
         while i < delta:
-            whole.append(".")
+            whole.append(" ")
             i += 1
     for r, row in enumerate(whole):
         #print("row " + str(len(row)) + " wwidth " + str(wwidth))
@@ -109,10 +104,24 @@ def renderInto(whole, wwidth, wheight, part, ptop, pleft, pwidth, pheight):
         if (lenrow > wwidth):
             whole[r] = row[:wwidth]
         if (lenrow < wwidth):
-            whole[r] = row + "." * (wwidth - lenrow)
+            whole[r] = row + " " * (wwidth - lenrow)
+    # Insert the partial component.
+    if (pwidth > 0 and pheight > 0 and len(part) > 0):
+        for r, row in enumerate(whole):
+            if (r >= ptop and r < ptop + pheight and len(part) > r - ptop):
+                newRow = insertIntoString(row, part[r - ptop], pleft)
+                whole[r] = newRow
+
     return
 
 
 def escape_ansi(line):
+    """If we have some escape characters in a string, they would be counted by len() but not shown. Thus strip before getting len()."""
     ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
     return ansi_escape.sub('', line)
+
+def insertIntoString(fullString, newPart, startingAt):
+    prefix = fullString[:startingAt]
+    postfix = fullString[(startingAt + len(escape_ansi(newPart))) :]
+    newString = "".join([prefix, newPart, postfix])
+    return newString
