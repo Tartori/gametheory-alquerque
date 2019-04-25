798edcbe90599b1ad3ae727844b239144a3e5123
Marc Rey <roxxistic@gmail.com>
2019-04-25 11:06:43 +0200

    improves positioning

--- README.md
+++ README.md
@@ -4,6 +4,26 @@ An implementation of the alerquerque game in Python with an opponent that tries 
 
 Rules from <https://www.mastersofgames.com/rules/alquerque-rules.htm>
 
+## Original version of Checkers (according to Mr. Eckerle)
+
+Für das Projekt ist ein einfaches Spiel, wie beispielsweise die Urform von Dame, zu realisieren.
+
+Spielregeln:
+
+Das Spielbrett ist ein rechtwinkliges Gitter der Grösse 5 x 5. Jeder Gitterpunkt stellt eine mögliche Dameposition dar. Zu Beginn wird das Brett mit einer Initialstellung belegt (siehe unten). Anschliessend machen beide Spieler jeweils abwechselnd einen Zug, der darin besteht einen Damenstein auf ein beliebiges direkt benachbartes freies Feld zu ziehen oder einen gegnerischen Stein zu überspringen (ist möglich, falls das unmittelbar dahinterliegende Feld frei ist). Der übersprungene Stein ist geschlagen und wird vom Brett entfernt. Im Unterschied zu den heutigen Damevarianten dürfen die Steine in beliebiger Richtung ziehen (vorwärts, rückwärts, seitwärts oder auch diagonal). In einem Zug darf maximal ein Stein übersprungen werden (statt mehrmalige zusammenhängende Sprünge wie beim Checker). Es gibt auch keine Umwandlung der Damesteine bei Erreichen der gegnerischen Linie. Eine Partei gewinnt, wenn der Gegner nicht mehr ziehen, sei es weil er keine Steine mehr besitzt oder sei es weil er bockiert ist (d.h. in der aktuellen Stellung keinen zulässigen Zug mehr ausführen kann). In allen anderen Fällen ist das Spiel Unentschieden.
+
+```
+S    S    S    S    S
+S    S    S    S    S
+S    S    .    W    W
+W    W    W    W    W
+W    W    W    W    W
+```
+
+Für die Bewertung der Horizontknoten kann statt einer heuristischen Bewertungsfunktion auch ein Monte-Carlo-Test zum Einsatz kommen. Siehe folgenden Artikel:
+
+Implementationsvorschlag: Querkle.
+
 ## Alquerque based on the Alfonso Manuscript
 
 ### Equipment
@@ -35,3 +55,9 @@ A draw occurs by agreement at any point during the game.  If it becomes apparent
 ### Alterations
 
 After a capture the capturing player can't continue. There are only single captures allowed at a time.
+
+## Running the code
+
+### Requirements
+
+- "Colorama" for cross platform coloring in terminal. See: <https://github.com/tartley/colorama>
--- gui.py
+++ gui.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
 
+from pip._vendor.colorama import init, Fore, Style
+import re
+
 # for demo state only!!!
 demoboard = [
-        [-1, 0, -1, 0, -1, 0, -1, 0, -1],
-        [0, -1, 0, -1, 0, -1, 0, -1, 0],
-        [-1, 0, -1, 0, -1, 0, -1, 0, -1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [0, 1, 0, 1, 0, 1, 0, 1, 0],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1],
+        [-1, -1, 0, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
     ]
 
 def printScreen(positions):
     """"""
+    init()
     lines = assembleComponents(positions)
     for line in lines:
         print(line)
     return
 
 def assembleComponents(positions):
+    layout = [
+        ""
+    ]
     screen = []
+    padding = 100
+    initialpadding = 0
+    while initialpadding < padding:
+        screen.append("")
+        initialpadding = initialpadding + 1
     title = prepareTitle()
     for i in title:
         screen.append(i)
     board = prepareBoard(positions)
     for i in board:
         screen.append(i)
+    renderInto(screen, 150, padding + 30, [], 0, 0, 0, 0)
     return screen
 
 def prepareTitle():
@@ -42,17 +52,22 @@ def prepareTitle():
 
 def prepareBoard(positions):
     """"""
-    colLabels    = "     0   1   2   3   4   5   6   7  "
-    rowSeparator = "   +---+---+---+---+---+---+---+---+"
+    colLabels    = "     0   1   2   3   4"
+    rowSeparator = "   +---+---+---+---+---+"
+    rowConnector = "   + | ╳ | ╳ | ╳ | ╳ | +"
+    conn = [
+        "╲\|/",
+        ""
+    ]
     board = []
     board.append(colLabels)
     board.append(rowSeparator)
-    for rowindex in range(0, 8):
+    for rowindex in range(0, 5):
         cells = []
         cells.append(getRowLabel(rowindex)) # one char in length
-        for cellindex in range(0, 8):
+        for cellindex in range(0, 5):
             cells.append(prepareCell(positions[rowindex][cellindex])) # one char in length
-        row = (" " + ' | '.join(['%s']*len(cells)) + " |") % tuple(cells)
+        row = (" " + ' ¦ '.join(['%s']*len(cells)) + " ¦") % tuple(cells)
         board.append(row)
         board.append(rowSeparator)
     return board
@@ -62,13 +77,42 @@ def getRowLabel(rowindex):
 
 def prepareCell(value):
     options = {
-        - 1: "O",
+        - 1: Fore.RED + "■" + Fore.RESET,
         0: " ",
-        1: "X"
+        1: Fore.CYAN + "■" + Fore.RESET
     }
     return options.get(value, "?")
 
 
 def prepareHistory():
     """"""
+    return [
+        "E0 to B4",
+        "something",
+        "K5 to W22             "
+    ]
+
+def renderInto(whole, wwidth, wheight, part, ptop, pleft, pwidth, pheight):
+    """"""
+    # if whole does not comply to the defined width and height, then extend or shorten
+    if (len(whole) > wheight):
+        whole = whole[:wheight]
+    if (len(whole) < wheight):
+        delta = wheight - len(whole)
+        i = 0
+        while i < delta:
+            whole.append(".")
+            i += 1
+    for r, row in enumerate(whole):
+        #print("row " + str(len(row)) + " wwidth " + str(wwidth))
+        lenrow = len(escape_ansi(row))
+        if (lenrow > wwidth):
+            whole[r] = row[:wwidth]
+        if (lenrow < wwidth):
+            whole[r] = row + "." * (wwidth - lenrow)
     return
+
+
+def escape_ansi(line):
+    ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', line)
