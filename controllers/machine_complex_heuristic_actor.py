fb8fc45c01c6c478dd1e16002a17d94f6a9d95fb
Marc Rey <roxxistic@gmail.com>
2019-06-19 17:49:17 +0200

    implements complex heuristic.

--- controllers/machine_complex_heuristic_actor.py
+++ controllers/machine_complex_heuristic_actor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from controllers import MachineABPruningActor
+from copy import deepcopy
+from models import Player
 
 
 class MachineComplexHeuristicActor(MachineABPruningActor):
@@ -13,4 +15,55 @@ class MachineComplexHeuristicActor(MachineABPruningActor):
 
     def _get_heuristic(self, game):
         # TODO: implement heuristic!!!
-        pass
+
+        # The more pawns one has compared to the number of pawns
+        # the opponent has, the better.
+        count_delta = self._get_fields_delta(game._get_bord(),
+                                             game._current_player)
+
+        # The further advanced a pawn, the better.
+        adv_board = deepcopy(game._get_bord())
+        size = game._size
+
+        for r in range(size):
+            for c in range(size):
+                if adv_board[r][c] == Player.OPP:
+                    # best row is at the "bottom" of the board
+                    # -1 to prevent the first move to always be two steps.
+                    # squared weight
+                    adv_board[r][c] *= (r - 1)**2
+                elif adv_board[r][c] == Player.USER:
+                    # best row is at the "top" of the board
+                    adv_board[r][c] *= (size - 1 - r - 1)**2
+
+        adv_delta = self._get_fields_delta(adv_board, game._current_player)
+
+        # A pawn that has an unblocked path to the finish line is great
+        board = game._get_bord()
+        top_field_value = [0] * size
+        btm_field_value = [0] * size
+
+        for r in range(size):
+            for c in range(size):
+                if board[r][c] == Player.OPP:
+                    if top_field_value[c] == 0:
+                        top_field_value[c] = Player.OPP
+                    btm_field_value[c] = Player.OPP
+
+                elif board[r][c] == Player.USER:
+                    if top_field_value[c] == 0:
+                        top_field_value[c] == Player.USER
+                    btm_field_value[c] = Player.USER
+
+        count_free_cols_usr = 0
+        count_free_cols_opp = 0
+        for p in top_field_value:
+            if p == Player.USER:
+                count_free_cols_usr += Player.USER
+        for p in btm_field_value:
+            if p == Player.OPP:
+                count_free_cols_opp += Player.OPP
+
+        free_row_delta = (count_free_cols_opp + count_free_cols_usr) * 10
+
+        return adv_delta + count_delta + free_row_delta
