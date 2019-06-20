061e942070f16c1b9622eb9551f1537dd1e1a9d0
Marc Rey <roxxistic@gmail.com>
2019-06-20 06:13:31 +0200

    only iterates once.

--- controllers/machine_complex_heuristic_actor.py
+++ controllers/machine_complex_heuristic_actor.py
@@ -14,14 +14,18 @@ class MachineComplexHeuristicActor(MachineABPruningActor):
         super().__init__(name, state, playerId)
 
     def _get_heuristic(self, game):
+        board = game._get_bord()
+        player = game._current_player
+        size = game._size
+
         # The more pawns one has compared to the number of pawns
         # the opponent has, the better.
-        count_delta = self._get_fields_delta(game._get_bord(),
-                                             game._current_player)
+        count_delta = self._get_fields_delta(board, player)
 
         # The further advanced a pawn, the better.
-        adv_board = deepcopy(game._get_bord())
-        size = game._size
+        adv_board = deepcopy(board)
+        top_field_value = [0] * size
+        btm_field_value = [0] * size
 
         for r in range(size):
             for c in range(size):
@@ -30,29 +34,21 @@ class MachineComplexHeuristicActor(MachineABPruningActor):
                     # -1 to prevent the first move to always be two steps.
                     # squared weight
                     adv_board[r][c] *= (r - 1) * (r - 1)
+
+                    if top_field_value[c] == 0:
+                        top_field_value[c] = Player.OPP
+                    btm_field_value[c] = Player.OPP
                 elif adv_board[r][c] == Player.USER:
                     # best row is at the "top" of the board
                     adv_board[r][c] *= (size - 1 - r - 1) * (size - 1 - r - 1)
 
-        adv_delta = self._get_fields_delta(adv_board, game._current_player)
-
-        # A pawn that has an unblocked path to the finish line is great
-        board = game._get_bord()
-        top_field_value = [0] * size
-        btm_field_value = [0] * size
-
-        for r in range(size):
-            for c in range(size):
-                if board[r][c] == Player.OPP:
-                    if top_field_value[c] == 0:
-                        top_field_value[c] = Player.OPP
-                    btm_field_value[c] = Player.OPP
-
-                elif board[r][c] == Player.USER:
                     if top_field_value[c] == 0:
                         top_field_value[c] == Player.USER
                     btm_field_value[c] = Player.USER
 
+        adv_delta = self._get_fields_delta(adv_board, player)
+
+        # A pawn that has an unblocked path to the finish line is great
         count_free_cols_usr = 0
         count_free_cols_opp = 0
         for p in top_field_value:
