517671a514e4537a307ff9012d1e1b7278cbdb3e
Marc Rey <roxxistic@gmail.com>
2019-06-20 08:01:02 +0200

    should much improve performance.

--- controllers/machine_complex_heuristic_actor.py
+++ controllers/machine_complex_heuristic_actor.py
@@ -27,49 +27,33 @@ class MachineComplexHeuristicActor(MachineABPruningActor):
         usr_now_blocked = [False] * size
         opp_now_blocked = [False] * size
 
-        for r in range(size):
+        for r in range(size - 1, -1, -1):
             for c in range(size):
                 # row indices for both perspectives.
                 r_opp = r
                 r_usr = size - 1 - r
 
-                opp_blocked = False
-                usr_blocked = False
+                # Perspective of user.
+                if board[r_usr][c] == Player.OPP:
+                    usr_now_blocked[c] = True
+                elif board[r_usr][c] == Player.USER:
+                    # best row is at the "top" of the board
+                    adv_board[r_usr][c] *= (r_usr - 1) * (r_usr - 1)
 
+                    if not usr_now_blocked[c]:
+                        adv_board[r_usr][c] *= 10  # TODO: choose best weight
 
-#                for preview in range(0, size):
-#                    if not usr_blocked and preview < r and board[r][c] == Player.OPP:
-#                        usr_blocked = True
-#                    elif not opp_blocked and preview > r and board[r][c] == Player.USR:
-#                        opp_blocked = True
-
-                if adv_board[r_opp][c] == Player.OPP:
-                    # check if blocked
-                    for preview in range(r + 1, size - 1):
-                        if board[preview][c] == Player.USER:
-                            opp_blocked = True
-                            break
-
-                    if not opp_blocked:
-                        adv_board[r_opp][c] *= 10  # TODO: choose best weight
-
+                # Perspective of opp.
+                if board[r_opp][c] == Player.USER:
+                    opp_now_blocked[c] = True
+                elif board[r_opp][c] == Player.OPP:
                     # best row is at the "bottom" of the board
                     # -1 to prevent the first move to always be two steps.
                     # squared weight
                     adv_board[r_opp][c] *= (r_opp - 1) * (r_opp - 1)
 
-                elif adv_board[r_usr][c] == Player.USER:
-                    # check if blocked
-                    for preview in range(1, r - 1):
-                        if board[preview][c] == Player.OPP:
-                            usr_blocked = True
-                            break
-
-                    if not usr_blocked:
-                        adv_board[r_usr][c] *= 10  # TODO: choose best weight
-
-                    # best row is at the "top" of the board
-                    adv_board[r_usr][c] *= (r_usr - 1) * (r_usr - 1)
+                    if not opp_now_blocked[c]:
+                        adv_board[r_opp][c] *= 10  # TODO: choose best weight
 
         adv_delta = self._get_fields_delta(adv_board, player)
 
